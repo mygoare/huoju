617b9739dab59d80595d826c173aa4520e84f3d0
mygoare <mygoare@gmail.com>
2014-10-05 11:34:42 +0800

    home controller root, about, contact, privacy, terms static pages

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,17 @@
+class HomeController < ApplicationController
+  def index
+    @home = 'Home page'
+  end
+
+  def about
+  end
+
+  def contact
+  end
+
+  def terms
+  end
+
+  def privacy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+
+  root 'home#index'
+  get 'about' => 'home#about'
+  get 'contact' => 'home#contact'
+  get 'terms' => 'home#terms'
+  get 'privacy' => 'home#privacy'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
