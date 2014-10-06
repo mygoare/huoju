f00c004066e25ccb9234b55428763d30884a7b00
mygoare <mygoare@gmail.com>
2014-10-06 12:13:19 +0800

    table association. event belongs_to user

--- db/migrate/20141004085530_create_users.rb
+++ db/migrate/20141004085530_create_users.rb
@@ -1,12 +1,12 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-    	t.string	:email
+    	t.string  :email
       t.boolean :email_valid
     	t.string	:user_name
     	t.string	:pwd
     	t.string 	:avatar
-    	t.boolean	:is_valid
+    	t.boolean :is_valid
     	t.text		:profile
     	t.string	:sex
 
--- db/migrate/20141004085811_create_events.rb
+++ db/migrate/20141004085811_create_events.rb
@@ -1,13 +1,14 @@
 class CreateEvents < ActiveRecord::Migration
   def change
     create_table :events do |t|
-    	t.string	:title
-    	t.text		:content
-    	t.string	:category
-    	t.string	:cover_image
-    	t.boolean	:is_valid
-    	t.integer	:favor
-    	t.string	:address
+    	t.string			:title
+    	t.text				:content
+    	t.string			:category
+    	t.string			:cover_image
+    	t.boolean			:is_valid
+    	t.integer			:favor
+    	t.string			:address
+    	t.belongs_to	:user
 
       t.timestamps
     end
