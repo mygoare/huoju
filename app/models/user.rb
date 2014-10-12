3313fdb102259233158ed6276c9dea8a8fa610b2
mygoare <mygoare@gmail.com>
2014-10-12 14:31:06 +0800

    model associations & validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ActiveRecord::Base
+  has_many :events, dependent: :destroy
+
+  validates :user_name, :email, :pwd, presence: true
+  validates :user_name, :email, uniqueness: true
+
 end
