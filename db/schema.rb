84322243b66bf49dbeed69ccc933e460b247be95
mygoare <mygoare@gmail.com>
2014-10-12 21:49:19 +0800

    add pwd_salt to User

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141011062344) do
+ActiveRecord::Schema.define(version: 20141012095801) do
 
   create_table "events", force: true do |t|
     t.string   "title"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20141011062344) do
     t.string   "sex",         default: "male"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "pwd_salt"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
