9418ee17f028cbf4c237714dfeb830002b8dca4f
mygoare <mygoare@gmail.com>
2014-10-20 18:55:04 +0800

    update how to create user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,15 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)
+    user_params = params[:user]
+    pwd_salt = BCrypt::Engine.generate_salt
+    pwd_hash = BCrypt::Engine.hash_secret(user_params[:pwd], pwd_salt)
+
+    @user = User.new
+    @user.user_name = user_params[:user_name]
+    @user.email = user_params[:email]
+    @user.pwd_salt = pwd_salt
+    @user.pwd_hash = pwd_hash
 
     if @user.save
       session[:user_id] = @user.id
@@ -30,10 +38,7 @@ class UsersController < ApplicationController
 
   end
 
-  def change_pwd
-    user_id         = params[:user_id]
-    user            = User.find(user_id)
-
+  def change_pwd user
     pwd_hash        = user.pwd_hash
     pwd_salt        = user.pwd_salt
 
@@ -42,22 +47,22 @@ class UsersController < ApplicationController
     new_pwd         = params[:new_pwd]
     confirm_new_pwd = params[:confirm_new_pwd]
 
-    if (current_pwd.empty? or new_pwd.empty? or confirm_new_pwd.empty?)
+    if current_pwd.empty? or new_pwd.empty? or confirm_new_pwd.empty?
       flash[:error] = "不得为空,所有都是必填项"
       redirect_to :back and return
     end
 
-    if (new_pwd != confirm_new_pwd)
+    if new_pwd != confirm_new_pwd
       flash[:error] = "两次秘密输入不一样"
       redirect_to :back and return
     end
 
-    if ( pwd_hash != BCrypt::Engine.hash_secret(current_pwd, pwd_salt) )
+    if pwd_hash != BCrypt::Engine.hash_secret(current_pwd, pwd_salt)
       flash[:error] = "原密码输入不正确"
       redirect_to :back and return
     end
 
-    if user.update(pwd: new_pwd)
+    if user.update(pwd_hash: BCrypt::Engine.hash_secret(new_pwd, pwd_salt))
       flash[:notice] = "修改密码成功"
       redirect_to :back
     else
@@ -65,16 +70,41 @@ class UsersController < ApplicationController
       redirect_to :back
 
     end
-
-
-
-
   end
 
-  private
+  def update_profile user
+    user_name = params[:user_name]
+    profile = params[:profile]
+    sex = params[:sex]
 
-  def user_params
-    params.require(:user).permit(:user_name, :email, :pwd)
+    if user.update(user_name: user_name, profile: profile, sex: sex)
+      flash[:notice] = "更新资料成功"
+      redirect_to :back
+    else
+      flash[:error] = "更新资料失败"
+      redirect_to :back
+    end
+  end
+
+  # /user/profile/edit
+  def profile
+    user_id         = session[:user_id]
+    user            = User.find(user_id)
+
+    if request.method == 'GET'
+      @user_name = user.user_name
+      @profile = user.profile
+      @sex = user.sex
+    end
+
+
+    if request.method == 'POST'
+      if params[:by] and params[:by] == 'pwd'
+        change_pwd user
+      else
+        update_profile user
+      end
+    end
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,6 @@ class User < ActiveRecord::Base
   validates :user_name, :email, presence: true
   validates :user_name, :email, uniqueness: true
 
-  attr_accessor :pwd
-  before_save :encrypt_password
-  def encrypt_password
-    self.pwd_salt = BCrypt::Engine.generate_salt
-    # raise "#{pwd}, #{pwd_salt}, #{self.inspect}"
-    self.pwd_hash = BCrypt::Engine.hash_secret(pwd, pwd_salt)
-  end
-
   def self.authenticate(email, pwd)
     user = User.find_by(email: email)
     # raise BCrypt::Engine.hash_secret(pwd, user.pwd_salt)
